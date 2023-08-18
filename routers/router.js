9583ac1dcfe98719d3b4fd64aa59fad347fae4c3
parulloyal <139440794+parulloyal@users.noreply.github.com>
2023-08-18 18:10:55 +1000

    SIT725_PRAC05
    -MVC Model

---
+++ routers/router.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let router = express.Router();
+let controller = require('../controllers/controller');
+
+router.post('/', (req, res) => {
+  // ask controller to perform logic
+controller.postCat(req, res);
+
+});
+
+router.get('/', (req, res) => {
+     // ask controller to perform logic
+     controller.getAllCats(req, res);
+
+});
+
+
+module.exports = router;
